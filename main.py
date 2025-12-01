a02fee6a0e915644a6ed9ec61d2987fd914a6d05
juanjoseecheverryfranco <juanjoseecheverryfranco@gmail.com>
2025-12-01 10:05:57 -0500

    Proyecto IA

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import google.generativeai as genai
 
 # Variables de entorno
 from dotenv import load_dotenv
-dotenv_path = os.path.join(os.path.dirname(_file_), ".env")
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path, override=True)
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
